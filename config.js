9d5dd9d0414b56fd2949d20109a5eaf9248707e5
Raesquiv13 <ricardesq3@gmail.com>
2020-04-06 15:29:41 -0600

    update the environment variable of the mongo db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 8000,
-    db: process.env.MONGODB || 'mongodb://127.0.0.1:27017/shop',
+    db: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/shop',
     SECRET_TOKEN: 'myclavedetokens'
 }
