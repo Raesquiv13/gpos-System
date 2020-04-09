143d0df83a71d31b49d4ed7de69485dfe6259a69
Raesquiv13 <ricardesq3@gmail.com>
2020-04-08 19:10:34 -0600

    Implement unit testing to validate the email format

--- spec/support/services/user_test_spec.js
+++ spec/support/services/user_test_spec.js
@@ -2,8 +2,57 @@ var userService = require('../../../services/users')
 
 describe('Validate email format', () => {
     it('Using a correct email', () => {
+        //Given:
         var email = "perroloco@email.com"
-        expect(userService.validateEmail(email)).toBeTrue()
+        var expectedResult = true
+        
+        //When:
+        var actualResult = userService.validateEmail(email)
+        
+        //Then:
+        expect(actualResult).toBe(expectedResult)
+    })
+
+    it('Using a incorrect email', () => {
+        //Given:
+        var email = "perro loco de la casa"
+        var expectedResult = false
+        
+        //When:
+        var actualResult = userService.validateEmail(email)
+        
+        //Then:
+        expect(actualResult).toBe(expectedResult)
+    })
+
+    it('Using a list of different emails', () => {
+        //Given:
+        var emailList = [
+            "forsberg@yahoo.com",
+            "pkilab@icloud.com",
+            "kludge@gmail.com",
+            "lstein@hotmail.com",
+            "carroll@mac.com",
+            "demmel@me.com",
+            "joelw@outlook.com",
+            "rhavyn@comcast.net",
+            "arachne@msn.com"
+        ]
+        var expectedResult = true
+        var actualResult
+        var validationList = []
+
+        //When:
+        emailList.forEach(email => {
+            actualResult = userService.validateEmail(email)
+            validationList.push(actualResult)
+        });
+
+        //Then
+        validationList.forEach(result => {
+            expect(result).toBe(expectedResult)
+
+        })
     })
 
 })
